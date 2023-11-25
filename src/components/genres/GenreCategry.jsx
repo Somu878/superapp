f803e6ebcbd0632e0fd675afae01cca791724fc7
somu878 <somumydhili19@gmail.com>
2023-11-25 16:37:25 +0530

    updated genres page

--- src/components/genres/GenreCategry.jsx
+++ src/components/genres/GenreCategry.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./genrecategory.module.css";
 import action from "..//../assets/image 2.png";
 import drama from "..//../assets/image 3.png";
@@ -9,6 +10,7 @@ import horror from "..//../assets/image 8.png";
 import fantasy from "..//../assets/image 9.png";
 import music from "..//../assets/image 10.png";
 import fiction from "..//../assets/image 11.png";
+import Tag from "./Tag";
 function GenreCategry() {
   const genreData = [
     {
@@ -66,24 +68,58 @@ function GenreCategry() {
       image: fiction,
     },
   ];
+  const navigate = useNavigate()
+  const [categories,Setcategories]= useState([])
+  const[minErr,setminErr] = useState(false)
+  const handleNextpagebtn=()=>{
+    if(categories.length<3){
+      setminErr(true)
+      return
+    }
+    else{
+      setminErr(false)
+      setTimeout(()=>{
+        navigate("/dashboard")
+      },500)
+    }
+  }
   return (
     <div className={styles.genrecat}>
       <div className={styles.pickedcat}>
         <div className={styles.superapp}>Superapp</div>
         <p>Choose your entertainment category</p>
-        {/* <div className={styles.caterror}>
-          ⚠ Minimum 3 categoies are required to proceed
-        </div> */}
+        <Tag data={categories} setData={Setcategories}/>
+        {
+          minErr?         <div className={styles.caterror}>
+          ⚠ Minimum 3 categories are required to proceed
+        </div>: <></>
+        }
       </div>
       <div className={styles.categories}>
         {genreData.map((item) => (
-          <CategoryCard data={item} key={item.id} />
+          <CategoryCard data={item} key={item.id}  categories={categories} Setcategories={Setcategories}/>
         ))}
       </div>
+      <button className={styles.nextpageBtn} onClick={handleNextpagebtn}>Next Page</button>
     </div>
   );
 }
-const CategoryCard = ({ data }) => {
+const CategoryCard = ({ data,categories,Setcategories }) => {
+  const [selected,setSelected] = useState()
+  const handleClick = (e) => {
+    if (categories.includes(data.category)) {
+      const id = categories.indexOf(data.category);
+      categories.splice(id, 1);
+      Setcategories([...categories]);
+    } else {
+      Setcategories([...categories, data.category]);
+    }
+    setSelected(!selected)
+  };
+  
+  useEffect(() => {
+    setSelected(categories.includes(data.category) == true);
+  });
   return (
     <div
       data={data}
@@ -92,7 +128,10 @@ const CategoryCard = ({ data }) => {
         color: "white",
         borderRadius: "10px",
         padding: "7px",
+        cursor:'pointer',
+        border: `${selected ? "4px solid green" : "4px solid transparent"}`,
       }}
+      onClick={(e)=>handleClick(e)}
     >
       <p style={{ padding: "6px", fontSize: "22px" }}>{data.category}</p>
       <img
--- src/components/genres/Tag.jsx
+++ src/components/genres/Tag.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
-function Tag() {
+function Tag({ data, setData }) {
   return (
-    <div>tag</div>
-  )
+    <div style={{ width: "40vw" }}>
+      {data.map((item) => (
+        <button
+          style={{
+            backgroundColor: "green",
+            fontSize: "17px",
+            color: "white",
+            width: "135px",
+            height: "40px",
+            borderRadius: "20px",
+            border: "none",
+            margin: "10px",
+            padding: "5px",
+            cursor: "pointer",
+            flexShrink: "0",
+          }}
+        >
+          {item}{" "}
+          <span
+            style={{ paddingLeft: "30px", color: "#292929",}}
+            onClick={() => {
+              const updatedData = data.filter((tag) => tag !== item);
+              setData(updatedData);
+            }}
+          >
+            X
+          </span>
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default Tag
+export default Tag;
