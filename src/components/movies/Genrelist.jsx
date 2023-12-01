e5d931614a50097ac6b11408970a9761e379103c
somu878 <somumydhili19@gmail.com>
2023-12-01 21:25:51 +0530

    added moviedatabase api

--- src/components/movies/Genrelist.jsx
+++ src/components/movies/Genrelist.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function Genrelist() {
+function Genrelist({ genre }) {
+  const [genreList, setGenreList] = useState([]);
+  useEffect(() => {
+    const options = {
+        method: 'GET',
+        url: `https://moviesdatabase.p.rapidapi.com/titles?genre=${genre}&year=2020`,
+        headers: {
+          'X-RapidAPI-Key': 'd19e1263e3msh3991af654d5761fp11dbeejsn713a6d266677',
+          'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+      };
+      
+      const fetchGenres= async ()=>{
+        try {
+            const response = await axios.request(options);
+            setGenreList(response.data.results.splice(4,4))
+        } catch (error) {
+            console.error(error);
+        }
+      }
+    fetchGenres();
+  }, []); 
+
   return (
-    <div>hello</div>
-  )
+    <>
+      <p style={{ overflowY: 'hidden' }}>{genre}</p>
+      <div style={{ display: 'flex', overflow: 'hidden', marginLeft: '2vw' }}>
+       {
+        genreList.map((item)=>{
+            return (
+                <div style={{ width: "20vw", margin: "2vw" }}>
+                <img style={{
+                  objectFit: "cover",
+                  width: "20vw",
+                  height: "15vh",
+                  borderRadius: "12px",
+                }} src={item?.primaryImage?.url} alt="imga" />
+                </div>
+            )
+        })
+       }
+      </div>
+    </>
+  );
 }
 
-export default Genrelist
+export default Genrelist;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import profileImg from '..//assets/image 14.png'
+import Genrelist from '../components/movies/Genrelist'
+
 function Movies() {
+    const genres = JSON.parse(window.localStorage.getItem('SelectedGenres'))
   return (
-    <div style={{overflow:'visible'}}>
+    <div style={{overflow:'visible', width: "100vw",
+    minHeight: "100vh",maxHeight: "100vh",}}>
         <img style={{
             position:'absolute',
             top:'5vh',
@@ -20,7 +24,11 @@ function Movies() {
                 fontSize:'30px',
                 fontFamily:'var(--dmsans)'
             }}>Entertainment according to your choice</p>
-            
+            {
+                genres.map((item)=>(
+                    <Genrelist genre={item}/>
+                ))
+            }
     </div>
   )
 }
